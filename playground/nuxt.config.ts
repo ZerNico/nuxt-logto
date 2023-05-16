8c30a221d3799d66bd7cc2358759557ea19d33bd
ZerNico <nico.franke01@gmail.com>
2023-05-16 16:34:15 +0200

    chore: load playground variables from env

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,13 +2,13 @@ export default defineNuxtConfig({
   modules: ['../src/module'],
   ssr: true,
   logto: {
-    appId: '<your-application-id>',
-    appSecret: '<your-app-secret-copied-from-console>',
-    endpoint: '<your-logto-endpoint>',
-    origin: '<your-nextjs-app-origin>',
+    appId: process.env.LOGTO_APP_ID,
+    appSecret: process.env.LOGTO_APP_SECRET,
+    endpoint: process.env.LOGTO_ENDPOINT,
+    origin: 'http://localhost:3000',
     basePath: '/api/logto',
     cookieSecret: 'complex_password_at_least_32_characters_long',
     cookieSecure: process.env.NODE_ENV === 'production',
-    resources: ['<your-resource-id>'],
+    resources: [],
   },
 })
